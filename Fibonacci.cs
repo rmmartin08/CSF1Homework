8eebc406e24e566aa6f57c9c1914dff2160be14e
Ryan.Martin <Ryan.Martin@VUHL-C2C01Z2>
2020-03-06 22:13:24 -0600

    added bonus items

---
+++ Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number you would like to go up to:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int numberOne = 0;
+            int numberTwo = 1;
+            int buffer;
+            Console.Write($"{numberOne}, ");
+            do
+            {
+                Console.Write($"{numberTwo}");
+                if (numberTwo < userInput)
+                {
+
+                    buffer = numberOne;
+                    numberOne = numberTwo;
+                    numberTwo += buffer;
+                }
+                if (numberTwo < userInput)
+                {
+                    Console.Write(", ");
+                }
+
+            } while (numberTwo < userInput);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= userNumber; i++)
+            {
+                Console.Write(i);
+                if (i >1 && i%3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if (i>5 && i%5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                if (i!=userNumber)
+                {
+                    Console.Write(", ");
+                }
+
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
